6d9d9c7a2632c2b4a77e6094deb9ae08953c71ec
Owen Mariani <mariani.owen@gmail.com>
2024-01-01 19:44:16 -0800

    finished projects and basic implementation.

---
+++ cv-website/src/helpers/ProjectList.js
@@ -0,0 +1,11 @@
+// remove this later when database set up
+import ToDoImage from "../assets/todo.png"
+
+export const ProjectList = [
+    {
+        name: "To-do App",
+        image: ToDoImage,
+        skills: "JavaScript, HTML, CSS",
+        url: "https://github.com/owenm-26/To-Do-List"
+    },
+]
---
+++ cv-website/src/pages/ProjectDisplay.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { ProjectList } from '../helpers/ProjectList';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import "../styles/ProjectDisplay.css"
+
+function ProjectDisplay() {
+    const {id} = useParams();
+    const project = ProjectList[id];
+  return (
+    <div className='project'>
+        <h1>{project.name}</h1>
+        <img src={project.image}/>
+        <p><b>Skills:</b> {project.skills}</p>
+        <a href={project.url}>
+        <GitHubIcon></GitHubIcon>
+        </a>
+    </div>
+  )
+}
+
+export default ProjectDisplay;
