fed008c14d6f7a60dad0a82c3d2a4f7cb5c14aee
Owen Mariani <mariani.owen@gmail.com>
2024-01-08 19:09:11 -0800

    added another project (currency converter)

--- cv-website/src/helpers/ProjectList.js
+++ cv-website/src/helpers/ProjectList.js
@@ -1,11 +1,18 @@
 // remove this later when database set up
 import ToDoImage from "../assets/todo.png"
+import CurrencyConverter from '../assets/currency-converter.png';
 
 export const ProjectList = [
     {
         name: "To-do App",
         image: ToDoImage,
-        skills: "JavaScript, HTML, CSS",
+        skills: "ReactJS, HTML, CSS",
         url: "https://github.com/owenm-26/To-Do-List"
+    },
+    {
+        name: "Currency Converter",
+        image: CurrencyConverter,
+        skills: "ReactJS, HTML, CSS",
+        url: "https://github.com/owenm-26/currency-exchange-rater"
     },
 ]
