f738b216730416d04fb7b9a4f8871354beae3360
Owen Mariani <mariani.owen@gmail.com>
2024-02-23 11:53:16 -0500

    added more projects

--- cv-website/src/helpers/ProjectList.js
+++ cv-website/src/helpers/ProjectList.js
@@ -1,8 +1,17 @@
 // remove this later when database set up
 import ToDoImage from "../assets/todo.png"
 import CurrencyConverter from '../assets/currency-converter.png';
+import TicTacToe from '../assets/tictactoe.png'
+import CSVCalculator from '../assets/csv-calculator.png'
+import AtScale from '../assets/atscale.png'
 
 export const ProjectList = [
+    {
+        name: "AtScale Summer 2023",
+        image: AtScale,
+        skills: "Python, PySpark, Snowflake, Databricks",
+        url: "https://github.com/owenm-26/AtScale-Internship-Summer-2023"
+    },
     {
         name: "To-do App",
         image: ToDoImage,
@@ -15,4 +24,16 @@ export const ProjectList = [
         skills: "ReactJS, HTML, CSS",
         url: "https://github.com/owenm-26/currency-exchange-rater"
     },
+    {
+        name: "Tic-Tac-Toe",
+        image: TicTacToe,
+        skills: "ReactJS, CSS",
+        url: "https://github.com/owenm-26/Tic-Tac-Toe"
+    },
+    {
+        name: 'CSV Calculator',
+        image: CSVCalculator,
+        skills: 'Python, command line',
+        url: "https://github.com/owenm-26/csv-calculator"
+    }
 ]
